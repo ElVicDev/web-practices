c6073341207bf73993e712df984099ff8d64f02b
ElVicDev <vicesc820@gmail.com>
2024-01-26 23:45:28 -0600

    First Object example

---
+++ META/JavaScript/6_Objets/storeManager.js
@@ -0,0 +1,18 @@
+//  storeManager
+
+var storeManager = {};
+storeManager.rangeTilesPerTurn = 4;
+storeManager.socialSkills = 50;
+storeManager.streetSmarts = 50;
+storeManager.health = 30;
+storeManager.specialAbility = "finding business opportunities";
+storeManager.gretting = "Let's make some money";
+
+//  Assistant Manager
+
+var assistantManager = {
+  movement: 3,
+  socialSkills: 30,
+  streetSmarts: 30,
+  healt: 40,
+};
