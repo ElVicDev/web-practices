53ac54edef616f73c511620fc97a07a5fc3e0f80
ElVicDev <vicesc820@gmail.com>
2024-01-28 14:01:29 -0600

    Math Objects examples

---
+++ META/JavaScript/6_Objets/mathCeil.js
@@ -0,0 +1,20 @@
+//  Math.ceil()
+
+//  Round any decimal number to the nearest integer
+var rounded = Math.ceil(0.0001);
+console.log(rounded);
+
+var rounded = Math.ceil(0.5);
+console.log(rounded);
+
+var rounded = Math.ceil(0.99);
+console.log(rounded);
+
+var rounded = Math.ceil(1.01);
+console.log(rounded);
+
+var rounded = Math.ceil(1.5);
+console.log(rounded);
+
+var rounded = Math.ceil(2.99);
+console.log(rounded);
