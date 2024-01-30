054e9228913f9af3a64bf70d07f01a7ff8aa48a8
ElVicDev <vicesc820@gmail.com>
2024-01-29 23:24:45 -0600

    Updating Syntax, logical and runtime errors

--- META/JavaScript/Bugs_and_errors/errors.js
+++ META/JavaScript/Bugs_and_errors/errors.js
@@ -13,14 +13,14 @@ Existen algunos otros errores en JavaScript.
 */
 
 //  *   ReferenceError
-console.log(username);
+//      console.log(username);
 /*  la variable llamadausername no ha sido declarada, 
     la línea de código anterior dará como resultado la siguiente salida:
     Uncaught ReferenceError: username is not defined
 */
 
 //  *   SyntaxError
-var a "there's no assignment operator here";
+//      var a "there's no assignment operator here";
 /*  Cualquier tipo de código JavaScript no válido provocará un SyntaxError.
     La línea de código anterior arrojará el siguiente error:
     Uncaught SyntaxError: Unexpected string
@@ -28,16 +28,16 @@ var a "there's no assignment operator here";
 */
 
 //  *   TypeError
-"hello".pop()
+//      "hello".pop()
 /*  Un TypeError se lanza cuando se intenta ejecutar un método 
     en un tipo de datos no soportado. Mostrará la siguiente salida:
     Uncaught TypeError: "hello".pop is not a function
 */
 
 //  *   RangeError
-//  Loas siguientes dos línea si se ejecutan:
-(10).toString(2); // '1010' Convierte un número de Base 10 a Base 2.
-(10).toString(8); // 12 Convierte un número base 10 a Base 8.
+//  Las siguientes dos línea si se ejecutan:
+//  (10).toString(2); // '1010' Convierte un número de Base 10 a Base 2.
+//  (10).toString(8); // 12 Convierte un número base 10 a Base 8.
 
 /*  Un RangeError se lanza cuando estamos dando un valor a una función, pero 
     ese valor está fuera del rango permitido de valores de entrada aceptables.
@@ -47,7 +47,7 @@ var a "there's no assignment operator here";
     inexistente está fuera delrango de los sistemas numéricos que están 
     disponibles para el método toString()
 */
-(10).toString(100);
+//      (10).toString(100);
 /*  La línea de código anterior arrojará el siguiente error:
     Uncaught RangeError: toString() radix argument must be between 2 and 36
 */
