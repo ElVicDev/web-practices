e3d019191368732143f58c7889b0d111a66c8a64
ElVicDev <vicesc820@gmail.com>
2024-02-01 23:38:41 -0600

    Introduction to JavaScript

---
+++ JavaScript/1_Conceptos_Basicos/1_Introduccion/usandoVar.js
@@ -0,0 +1,20 @@
+// Simple one dimensional array
+var vehicles = ["Ford", "Honda", "Toyota"];
+console.log(vehicles[1][2]);
+
+// Nested array
+var vehicles = [
+  "Ford",
+  ["Fiesta", "Taurus", "Explorer"],
+  "Honda",
+  ["Pilot", "Civic", "Accord"],
+  "Toyota",
+  ["Corolla", "Camry", "Prius"],
+];
+console.log(vehicles[1][2]);
+
+// ---------------------------------------------
+
+var joke = "The chicken crossed the road";
+console.log(joke[4]);
+console.log(joke.length);
