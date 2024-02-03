21883c25e354dc2e0b21bf059c6bb93155a6bcf4
ElVicDev <vicesc820@gmail.com>
2024-02-02 21:12:38 -0600

    Formas de declarar una variable y reglas

---
+++ JavaScript/1_Conceptos_Basicos/2_Variables/variables1.js
@@ -0,0 +1,28 @@
+//  *   FORMAS DE DECLARAR UNA VARIABLE
+
+//  Crea una variable llamada "x" y le asigna una cadena vacía.
+var x = "";
+
+//  Crea una variable llamada "chesse" y le asigna el valor "yummy"
+var cheese = "yummy";
+
+//  Crea un avariabla llamada age y le asigna el valor de 25
+var age = 25;
+
+//  *   REGLAS
+/*  Las variables distingen mayúsculas de minúsculas. 
+    A continuación 3 ejemplos:
+*/
+var cat = "this cat";
+var CAT = "that cat";
+var Cat = "another cat";
+
+/*  var 1cat = "";      //  Una variable tiene que empezar con una letra
+    var $cat = "";      //  $ es usado para librerías
+    var my-cat = "";    //  No podemos usar el renglon medio
+    var my cat = "";    //  No podemos usar espacios
+    var new = "";       //  new es una keyword. No se utiliza como variale
+*/
+
+//  Utilizar minúsculas al generar una variable o en 'camel case name'
+var myCat = "camel case name";
