f8f2fa0740d2a819effbfc569478e66883c77e4e
ElVicDev <vicesc820@gmail.com>
2024-03-11 02:52:34 -0600

    Updating challenge28.html file and adding script28.js file

---
+++ JavaScript/1_Conceptos_Basicos/Challenges_22_to_30/script28.js
@@ -0,0 +1,13 @@
+(function () {
+  "use strict";
+
+  const divTag = document.querySelector("div");
+
+  divTag.addEventListener("mouseover", function () {
+    divTag.className = "big";
+  });
+
+  divTag.addEventListener("mouseout", function () {
+    divTag.removeAttribute("class");
+  });
+})();
