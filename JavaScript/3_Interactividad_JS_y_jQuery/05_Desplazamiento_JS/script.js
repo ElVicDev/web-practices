f34b2ac1e27b0d9ef984a19053e31d890aec4567
ElVicDev <vicesc820@gmail.com>
2024-04-11 22:47:57 -0600

    Smooth Scroll - JavaScript

--- JavaScript/3_Interactividad_JS_y_jQuery/05_Desplazamiento_JS/script.js
+++ JavaScript/3_Interactividad_JS_y_jQuery/05_Desplazamiento_JS/script.js
@@ -22,23 +22,64 @@ window.addEventListener("load", function () {
   let prevCounter = 1;
   let doneResizing;
 
-  posts.forEach(function (post) {
-    postTops.push(
-      Math.floor(post.getBoundingClientRect().top + window.pageYOffset)
-    );
-  });
-  // console.log(postTops);
+  resetPagePosition();
 
   window.addEventListener("scroll", function () {
-    pageTop = window.pageYOffset;
+    pageTop = window.pageYOffset + 250;
     // console.log(pageTop) + 250;
 
     if (pageTop > postTops[counter]) {
       counter++;
-      console.log(`Scrolling down! ${counter}`);
+      // console.log(`Scrolling down! ${counter}`);
     } else if (counter > 1 && pageTop < postTops[counter - 1]) {
       counter--;
-      this.console.log(`Scrolling up! ${counter}`);
+      // console.log(`Scrolling up! ${counter}`);
+    }
+
+    if (counter != prevCounter) {
+      navLinks.forEach(function (eachLink) {
+        eachLink.removeAttribute("class");
+      });
+      const thisLink = document.querySelector(
+        `nav ul li:nth-child(${counter}) a`
+      );
+      thisLink.className = "selected";
+      prevCounter = counter;
     }
   });
+
+  window.addEventListener("resize", function () {
+    clearTimeout(doneResizing);
+    doneResizing = setTimeout(function () {
+      resetPagePosition();
+    }, 500);
+  });
+
+  function resetPagePosition() {
+    postTops = [];
+
+    posts.forEach(function (post) {
+      postTops.push(
+        Math.floor(post.getBoundingClientRect().top + window.pageYOffset)
+      );
+    });
+
+    const pagePosition = window.pageYOffset + 250;
+    counter = 0;
+
+    postTops.forEach(function (post) {
+      if (pagePosition > post) {
+        counter++;
+      }
+    });
+
+    navLinks.forEach(function (eachLink) {
+      eachLink.removeAttribute("class");
+    });
+
+    const thisLink = document.querySelector(
+      `nav ul li:nth-child(${counter}) a`
+    );
+    thisLink.className = "selected";
+  }
 });
