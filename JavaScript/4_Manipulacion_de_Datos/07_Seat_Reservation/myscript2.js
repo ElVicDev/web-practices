5f8028fa8283cd0cfe516f0fd18fa46943eaa620
ElVicDev <vicesc820@gmail.com>
2024-05-14 22:13:10 -0600

    Seat Reservation Program - Adding The reservedSeats Object

--- JavaScript/4_Manipulacion_de_Datos/07_Seat_Reservation/myscript2.js
+++ JavaScript/4_Manipulacion_de_Datos/07_Seat_Reservation/myscript2.js
@@ -1,3 +1,38 @@
+/*Datos de reserva de asiento
+  Se agregó este objeto en la parte superior de este archivo de secuencia de comandos 
+  del proyecto para proporcionar algunos datos iniciales de los asientos reservados 
+  previamente, que podrían provenir de una base de datos. */
+var reservedSeats = {
+  record1: {
+    seat: "b19",
+    owner: {
+      fname: "Joe",
+      lname: "Smith",
+    },
+  },
+  record2: {
+    seat: "b20",
+    owner: {
+      fname: "Joe",
+      lname: "Smith",
+    },
+  },
+  record3: {
+    seat: "b21",
+    owner: {
+      fname: "Joe",
+      lname: "Smith",
+    },
+  },
+  record4: {
+    seat: "b22",
+    owner: {
+      fname: "Joe",
+      lname: "Smith",
+    },
+  },
+};
+
 /* PARA LA CREACIÓN DE ASIETOS UTILIZAREMOS SWITCH QUE ES MAS COMPACTO. */
 
 function makeRows(sectionLength, rowLength, placement) {
@@ -72,3 +107,19 @@ function makeRows(sectionLength, rowLength, placement) {
 makeRows(3, 15, "left");
 makeRows(3, 15, "right");
 makeRows(9, 15, "middle");
+
+(function () {
+  "use strict";
+
+  for (const key in reservedSeats) {
+    /*Lidiando con la herencia prototípica
+      Envuelva el núcleo del bucle for en esta declaración if. 
+      Realmente no es un problema aquí, pero se considera una mejor práctica. */
+    if (reservedSeats.hasOwnProperty(key)) {
+      const obj = reservedSeats[key];
+      // console.log(obj.seat);
+      document.getElementById(obj.seat).className = "r";
+      document.getElementById(obj.seat).innerHTML = "R";
+    }
+  }
+})();
