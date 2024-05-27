203635ea2243db54d14612556918ae6046548d7f
ElVicDev <vicesc820@gmail.com>
2024-05-26 23:24:13 -0600

    Basic Contact Form - jQuery Form

--- JavaScript/4_Manipulacion_de_Datos/10_Contact_Form/01-jQuery-form/script.js
+++ JavaScript/4_Manipulacion_de_Datos/10_Contact_Form/01-jQuery-form/script.js
@@ -1,23 +1,49 @@
 // add script here
+var formDataUrl =
+  "https://cpe-web-assignments.ucdavis.edu/formprocessing/processor.php";
 
+$("#formdata").load(formDataUrl);
+
+$("#myForm").validate();
+
+$("#myForm").submit(function (event) {
+  event.preventDefault();
+
+  if ($("#myForm").valid() == true) {
+    var dataString = $(this).serialize();
+    // console.log(dataString);
+    $.ajax({
+      type: "POST",
+      url: formDataUrl,
+      data: dataString,
+      success: function (data) {
+        $("#formdata").html(data);
+        $("#myForm").clearForm();
+      },
+    });
+  }
+});
 
 $.fn.clearForm = function () {
-	return this.each(function () {
+  return this.each(function () {
+    var type = this.type;
+    var tag = this.tagName.toLowerCase();
 
-		var type = this.type;
-		var tag = this.tagName.toLowerCase();
-
-		if (tag == 'form') {
-			return $(':input', this).clearForm();
-		}
-		if (type == 'text' || type == 'password' || type == 'email' || type == 'url' || tag == 'textarea') {
-			this.value = '';
-		}
-		else if (type == 'checkbox' || type == 'radio') {
-			this.checked = false;
-		}
-		else if (tag == 'select') {
-			this.selectedIndex = -1;
-		}
-	});
+    if (tag == "form") {
+      return $(":input", this).clearForm();
+    }
+    if (
+      type == "text" ||
+      type == "password" ||
+      type == "email" ||
+      type == "url" ||
+      tag == "textarea"
+    ) {
+      this.value = "";
+    } else if (type == "checkbox" || type == "radio") {
+      this.checked = false;
+    } else if (tag == "select") {
+      this.selectedIndex = -1;
+    }
+  });
 };
