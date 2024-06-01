21da184af48628e68dd539799b4c2de5d1b9e579
ElVicDev <vicesc820@gmail.com>
2024-05-31 22:33:41 -0600

    Studio Ghibli - Part 1

--- JavaScript/4_Manipulacion_de_Datos/11_Studio_Ghibli/Part_1/script.js
+++ JavaScript/4_Manipulacion_de_Datos/11_Studio_Ghibli/Part_1/script.js
@@ -1,17 +1,47 @@
 const mainElement = document.querySelector("main");
+
+let filmData;
+
 async function getFilms() {
-  // Usar live Server en Visual Studio Code para obtener los datos de las películas
-  const filmsPromise = await fetch("./films.json");
+  const filmsPromise = await fetch("./movieData.json");
   const films = await filmsPromise.json();
 
-  //films.sort((a, b) => (a.title > b.title ? 1 : -1));
-  console.log(films);
-  /* films.forEach(function (eachFilm) {
-    createCard(eachFilm);
-  }); */
+  setSort(films);
+  addCards(films);
+  filmData = films;
+  document.getElementById("sortorder").removeAttribute("disabled");
 }
 
 getFilms();
+
+document.getElementById("sortorder").addEventListener("change", function () {
+  mainElement.innerHTML = "";
+  setSort(filmData);
+  addCards(filmData);
+});
+
+function setSort(array) {
+  const sortOrder = document.getElementById("sortorder").value;
+  switch (sortOrder) {
+    case "title":
+      array.sort((a, b) => (a.title > b.title ? 1 : -1));
+      break;
+    case "release_date":
+      array.sort((a, b) => (a.release_date > b.release_date ? 1 : -1));
+      break;
+    case "rt_score":
+      array.sort((a, b) =>
+        parseInt(a.rt_score) > parseInt(b.rt_score) ? -1 : 1
+      );
+      break;
+  }
+}
+
+function addCards(array) {
+  array.forEach((eachItem) => {
+    createCard(eachItem);
+  });
+}
 
 function createCard(data) {
   const card = document.createElement("article");
