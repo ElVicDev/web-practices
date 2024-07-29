3712f4f77fd8029b06d70fc33d7dbf49cd88a228
ElVicDev <vicesc820@gmail.com>
2024-07-28 22:02:28 -0600

    Example - var variable

---
+++ META/JavaScript/Functional_Programming_FP/VarKeyword.js
@@ -0,0 +1,15 @@
+//var keyword;
+
+/*  1. Podemos acceder a una variable var antes de la inicialización...
+    ... siempre que la variable var se inicialice finalmente
+    ... en cualquier parte de nuestro código */
+console.log(user);
+
+/*  2. Podemos declarar y redeclarar la misma variable var sin errores */
+var user = "Mary";
+var user = "Joanna";
+var user = "Mark";
+
+console.log(user);
+
+//var user;
