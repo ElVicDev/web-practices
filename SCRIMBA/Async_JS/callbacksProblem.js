b8d6924d44a29af26f415f91fa19c92786aff73c
ElVicDev <vicesc820@gmail.com>
2024-08-08 20:09:38 -0600

    The Problem with Callbacks

---
+++ SCRIMBA/Async_JS/callbacksProblem.js
@@ -0,0 +1,12 @@
+//  *** The problem with Callbacks
+
+navigator.geolocation.getCurrentPosition((position) => {
+  console.log(position);
+  getRestaurants(position, (restaurants) => {
+    console.log(restaurants);
+    console.log("done");
+  });
+});
+
+// setTimeout()
+// addEventListener()
