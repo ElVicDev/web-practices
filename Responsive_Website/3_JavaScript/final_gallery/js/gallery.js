128be5d1401d33275ffbf02b44ee230bb5b55813
ElVicDev <vicesc820@gmail.com>
2024-09-23 23:48:54 -0600

    Responsive Website - Photo Album - Final Gallery - Startup Files

--- Responsive_Website/3_JavaScript/final_gallery/js/gallery.js
+++ Responsive_Website/3_JavaScript/final_gallery/js/gallery.js
@@ -1,156 +1,131 @@
-
 /*
- *		This file contains the javascript code for our gallery
+ *		Este archivo contiene el código javascript para nuestra galería.
  */
 
-// variables for all of the templates so we only have to compile
-// them once on page load and can then use the same compiled 
-// templates many times
+/* 	Variables para todas las plantillas, de modo que solo tengamos que 
+	compilarlas una vez al cargar la página y luego podamos usar las 
+	mismas plantillas compiladas muchas veces. */
 var albums_template, photos_template, photo_template, slideshow_template;
 
-// variables to store the current displayed album and photo
+// Variables para almacenar el álbum y la foto mostrados actualmente.
 var current_album = gallery.albums[0];
 var current_photo = current_album.photos[0];
 
-// a helper function that instantiates a template
-// and displays the results in the content div
-function showTemplate(template, data){
-	var html    = template(data);
-	$('#content').html(html);
+/* 	Una función auxiliar que crea una plantilla y muestra los 
+	resultados en el contenido div. */
+function showTemplate(template, data) {
+  var html = template(data);
+  $("#content").html(html);
 }
 
-// document read gets called when the whole document
-// is loaded, so we put most of the code that needs to run
-// in here
-$(document).ready(function(){
+/* 	La lectura del documento se llama cuando se carga todo el documento, 
+	por lo que colocamos aquí la mayor parte del código que necesita ejecutarse. */
+$(document).ready(function () {
+  /* Recopila todas nuestras plantillas listas para usar. */
+  var source = $("#albums-template").html();
+  albums_template = Handlebars.compile(source);
 
-	//
-	// compile all of our templates ready for use
-	//
-	var source   = $("#albums-template").html();
-	albums_template = Handlebars.compile(source);
-	
-	source   = $("#photos-template").html();
-	photos_template = Handlebars.compile(source);
-	
-	source   = $("#photo-template").html();
-	photo_template = Handlebars.compile(source);
-	
-	source   = $("#slideshow-template").html();
-	slideshow_template = Handlebars.compile(source);
+  source = $("#photos-template").html();
+  photos_template = Handlebars.compile(source);
 
-	// 
-	//  clicking on the albums tab shows the 
-	//  thumbnails of all the albums
-	//
-	$("#albums-tab").click(function () {
+  source = $("#photo-template").html();
+  photo_template = Handlebars.compile(source);
 
-		// displays the albums template
-		showTemplate(albums_template, gallery);
+  source = $("#slideshow-template").html();
+  slideshow_template = Handlebars.compile(source);
 
-		// make the albums tab the active one
-		// first make the currently active tab inactive
-		$(".nav-tabs .active").removeClass("active");
-		// then make albums tab active
-		$("#albums-tab").addClass("active");
+  /* 	Al hacer clic en la pestaña de álbumes se muestran las miniaturas 
+		de todos los álbumes. */
+  $("#albums-tab").click(function () {
+    // Muestra la plantilla de álbumes.
+    showTemplate(albums_template, gallery);
 
-		// add a click callback to each album 
-		// thumbnail which displays the photos
-		// template on that album
-		// (I have written out the code for this 
-		// function for clarity but it is actually
-		// pretty much the same as the photos tab
-		// function so we could acutally just
-		// call $(".photo-thumbnail").click() ) 
-		$(".album-thumbnail").click(function (){
-			
-			// get the index (position in the array)
-			// of the album we clicked on
-			// "this" is the element that was clicked on
-			// data("id") gets the attribute data-id
-			// (which we set to the index of the album in
-			// the array - @index)
-			var index = $(this).data("id");
+    /* 	Primero haga que la pestaña de álbumes sea la activa 
+			y luego desactive la pestaña actualmente activa. */
+    $(".nav-tabs .active").removeClass("active");
+    // Luego, activa la pestaña de álbumes.
+    $("#albums-tab").addClass("active");
 
-			// set the current album to this album
-			current_album = gallery.albums[index];
+    /* 	Agregue una devolución de llamada de clic a cada miniatura de 
+		álbum que muestre la plantilla de fotos en ese álbum 
+		(he escrito el código para esta función para mayor claridad, 
+		pero en realidad es prácticamente lo mismo que la función de 
+		la pestaña de fotos, por lo que podríamos simplemente 
+		llamar a $(".photo-thumbnail").click() ). */
+    $(".album-thumbnail").click(function () {
+      /* 	Obtiene el índice (posición en la matriz) del álbum en el que 
+			hicimos clic. "this" es el elemento en el que se hizo clic. 
+			data("id") obtiene el atributo data-id (que establecemos en 
+			el índice del álbum en la matriz - @index). */
+      var index = $(this).data("id");
 
-			// displays the photos template
-			showTemplate(photos_template, current_album);
+      // Establezca el álbum actual en este álbum.
+      current_album = gallery.albums[index];
 
-			// add an on click al all the photo thumbnails
-			// which displays the photo in a modal popup
-			$(".photo-thumbnail").click(function (){
-				// get the index (position in the array)
-				// of the photo we clicked on
-				// "this" is the element that was clicked on
-				// data("id") gets the attribute data-id
-				// (which we set to the index of the photo in
-				// the array - @index)
-				var index = $(this).data("id");
+      // Muestra la plantilla de fotos.
+      showTemplate(photos_template, current_album);
 
-				// set the current photo to this photo
-				current_photo = current_album.photos[index];
-				
-				// displays the single photo template
-				showTemplate(photo_template, current_photo);
-			});
-		});
-	});
+      /* 	Agregue un clic en todas las miniaturas de fotos que muestra 
+				la foto en una ventana emergente modal. */
+      $(".photo-thumbnail").click(function () {
+        /* 	Obtiene el índice (posición en la matriz) de la foto en la que hicimos clic. 
+			"this" es el elemento en el que se hizo clic. 
+			data("id") obtiene el atributo data-id 
+			(que establecemos en el índice de la foto en la matriz - @index) */
+        var index = $(this).data("id");
 
-	// 
-	//  clicking on the photos tab shows all of the 
-	//  photos in the current album
-	//
-	$("#photos-tab").click(function () {
-		
-		// displays the photos template
-		showTemplate(photos_template, current_album);
+        // Establezca la foto actual en esta foto.
+        current_photo = current_album.photos[index];
 
-		// make the photos tab the active one
-		// first make the currently active tab inactive
-		$(".nav-tabs .active").removeClass("active");
-		// then make photos tab active
-		$("#photos-tab").addClass("active");
+        // Muestra la plantilla de fotografía única.
+        showTemplate(photo_template, current_photo);
+      });
+    });
+  });
 
-		// add an on click al all the photo thumbnails
-		// which displays the photo in a modal popup
-		$(".photo-thumbnail").click(function (){
-			// get the index (position in the array)
-			// of the photo we clicked on
-			// "this" is the element that was clicked on
-			// data("id") gets the attribute data-id
-			// (which we set to the index of the photo in
-			// the array - @index)
-			var index = $(this).data("id");
+  /* 	Al hacer clic en la pestaña de fotos se muestran 
+		todas las fotos del álbum actual. */
+  $("#photos-tab").click(function () {
+    // Muestra la plantilla de fotos.
+    showTemplate(photos_template, current_album);
 
-			// set the current photo to this photo
-			current_photo = current_album.photos[index];
-			
-			// displays the single photo template
-			showTemplate(photo_template, current_photo);
-		});
-	});
+    /* 	Primero haga que la pestaña de fotos sea la activa 
+			y luego desactive la pestaña actualmente activa. */
+    $(".nav-tabs .active").removeClass("active");
+    // Luego, activa la pestaña de fotos.
+    $("#photos-tab").addClass("active");
 
-	// 
-	//  clicking on the slideshow tab displays the
-	//  current album as a slide show
-	//
-	$("#slideshow-tab").click(function () {
-		// display the slideshow template using the 
-		// current album
-		showTemplate(slideshow_template, current_album);
-		
-		// make the slideshow tab the active one
-		// first make the currently active tab inactive
-		$(".nav-tabs .active").removeClass("active");
-		// then make slideshow tab active
-		$("#slideshow-tab").addClass("active");
-	});
+    /* 	Agregue un clic en todas las miniaturas de fotos que 
+			muestra la foto en una ventana emergente modal. */
+    $(".photo-thumbnail").click(function () {
+      /* Obtiene el índice (posición en la matriz) de la foto en la que hicimos clic. 
+		"this" es el elemento en el que se hizo clic. 
+		data("id") obtiene el atributo data-id 
+		(que establecemos en el índice de la foto en la matriz - @index). */
+      var index = $(this).data("id");
 
-	// start the page by showing the albums view
-	// we do this by virtually clicking on the 
-	// albums tab
-	$("#albums-tab").click();
+      // Establezca la foto actual en esta foto.
+      current_photo = current_album.photos[index];
 
+      // Muestra la plantilla de fotografía única.
+      showTemplate(photo_template, current_photo);
+    });
+  });
+
+  /* 	Al hacer clic en la pestaña de presentación de diapositivas, 
+		se muestra el álbum actual como una presentación de diapositivas. */
+  $("#slideshow-tab").click(function () {
+    // Muestra la plantilla de presentación de diapositivas utilizando el álbum actual.
+    showTemplate(slideshow_template, current_album);
+
+    /* 	Primero haga que la pestaña de presentación de diapositivas 
+			sea la activa y luego desactive la pestaña actualmente activa. */
+    $(".nav-tabs .active").removeClass("active");
+    // then make slideshow tab active
+    $("#slideshow-tab").addClass("active");
+  });
+
+  /* 	Para iniciar la página debemos mostrar la vista de álbumes, 
+		para ello debemos hacer clic virtualmente en la pestaña de álbumes. */
+  $("#albums-tab").click();
 });
