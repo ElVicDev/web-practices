e7c7c0cd0cfa2a2b3a3980bfa0c7554db6b36989
ElVicDev <vicesc820@gmail.com>
2024-09-27 23:06:32 -0600

    Reassigning and Incrementing

---
+++ SCRIMBA/JavaScript/02_math_operations/reassignIncrement.js
@@ -0,0 +1,15 @@
+/*  Crea una variable, bonusPoints. Inicialízala como 50. Auméntala a 100.
+    Disminúyela a 25 y, finalmente, auméntala a 70.
+    Registra el valor en console.log después de cada paso. */
+
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints - 75;
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 45;
+console.log(bonusPoints);
