e3425c6b2d9aedb9fcc79e27bfadb4754009800c
ElVicDev <vicesc820@gmail.com>
2024-10-02 00:21:23 -0600

    Build a Blackjack game - If...Else - Example 1

---
+++ SCRIMBA/JavaScript/05_if_else/ifElse1.js
@@ -0,0 +1,12 @@
+/*  Comprueba si la persona tiene la edad suficiente para entrar en la discoteca (21).
+    En ambos casos, registra un mensaje adecuado en la consola. */
+
+let age = 20;
+
+// if less than 21 -> "You can not enter the club!"
+if (age < 21) {
+  console.log("You can not enter the club! 😭");
+  // else            -> "Welcome!"
+} else {
+  console.log("Welcome! 🥳");
+}
