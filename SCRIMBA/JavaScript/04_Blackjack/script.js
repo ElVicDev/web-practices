cd696a30decf12775266d71070e7862094641e3a
ElVicDev <vicesc820@gmail.com>
2024-10-03 23:43:02 -0600

    Build a Blackjack game - Add the hasBlackJack variable

--- SCRIMBA/JavaScript/04_Blackjack/script.js
+++ SCRIMBA/JavaScript/04_Blackjack/script.js
@@ -3,10 +3,10 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6;
-let secondCard = 9;
-
+let firstCard = 10;
+let secondCard = 11;
 let sum = firstCard + secondCard;
+let hasBlackJack = false;
 
 // console.log(sum);
 
@@ -14,6 +14,10 @@ if (sum < 21) {
   console.log("Do you want to draw a new card? 🙂");
 } else if (sum === 21) {
   console.log("Wohoo! You've got Blackjack! 🥳");
+  hasBlackJack = true;
 } else {
   console.log("You're out of the game! 😭");
 }
+
+// CASH OUT!
+console.log(hasBlackJack);
