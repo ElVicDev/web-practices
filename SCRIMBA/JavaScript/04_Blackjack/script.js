0286438ff96607c131b7f5151c53c6a9770c29d0
ElVicDev <vicesc820@gmail.com>
2024-10-07 02:56:34 -0600

    Display the sum

--- SCRIMBA/JavaScript/04_Blackjack/script.js
+++ SCRIMBA/JavaScript/04_Blackjack/script.js
@@ -1,15 +1,17 @@
 let firstCard = 10;
-let secondCard = 10;
+let secondCard = 4;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-console.log(messageEl);
-
-// 1. Almacene el párrafo message-el en una variable llamada messageEl
+// 2. Almacene el párrafo de suma en una variable llamada sumEl
+let sumEl = document.getElementById("sum-el");
+//let sumEl = document.querySelector("body");
 
 function startGame() {
+  // 3. Representa la suma en la página usando este formato -> "Suma: 14"
+  sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
   } else if (sum === 21) {
@@ -19,6 +21,5 @@ function startGame() {
     message = "You're out of the game!";
     isAlive = false;
   }
-  // 2. Muestra el mensaje en messageEl usando messageEl.textContent
   messageEl.textContent = message;
 }
