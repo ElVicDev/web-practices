129da3099c75e6e6f0ee287da44b3d5fea097423
ElVicDev <vicesc820@gmail.com>
2024-10-07 03:08:02 -0600

    Display the cards

--- SCRIMBA/JavaScript/04_Blackjack/script.js
+++ SCRIMBA/JavaScript/04_Blackjack/script.js
@@ -5,12 +5,14 @@ let hasBlackJack = false;
 let isAlive = true;
 let message = "";
 let messageEl = document.getElementById("message-el");
-// 2. Almacene el párrafo de suma en una variable llamada sumEl
 let sumEl = document.getElementById("sum-el");
-//let sumEl = document.querySelector("body");
+// 2. Almacene el párrafo de tarjetas en una variable llamada cardsEl
+let cardsEl = document.getElementById("cards-el");
 
 function startGame() {
-  // 3. Representa la suma en la página usando este formato -> "Suma: 14"
+  // 3. Representa los coches en la página usando este formato -> "Tarjetas: 10 4"
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+  // Representa la suma en la página usando este formato -> "Suma: 14"
   sumEl.textContent = "Sum: " + sum;
   if (sum < 21) {
     message = "Do you want to draw a new card?";
