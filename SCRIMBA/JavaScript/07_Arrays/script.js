dbbe4ccf7de47a56263d0b461691ae46e6985e71
ElVicDev <vicesc820@gmail.com>
2024-10-08 21:35:01 -0600

    Intro to arrays

---
+++ SCRIMBA/JavaScript/07_Arrays/script.js
@@ -0,0 +1,19 @@
+// Matrices: listas ordenadas de elementos
+
+let featuredPosts = [
+  "Check out my Netflix clone",
+  "Here's the code for my project",
+  "I've just relaunched my portfolio",
+];
+
+// Crea una matriz que incluya tu experiencia.
+// Los elementos de la matriz deben ser cadenas.
+let experience = [
+  "Full Stack",
+  "Developer Apps",
+  "Freelance",
+  "SW/App/Cloud/Tech Support Sr. Engineer",
+  "Business Critical Solution Engineer",
+];
+
+console.log(experience);
