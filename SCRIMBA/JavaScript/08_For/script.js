963c23a14a581286452592a118a37eccf5274d70
ElVicDev <vicesc820@gmail.com>
2024-10-09 16:57:55 -0600

    Counting in JavaScript

---
+++ SCRIMBA/JavaScript/08_For/script.js
@@ -0,0 +1,12 @@
+// ¡Cuenta hasta diez!
+
+// Necesitamos especificar...
+
+// ¿Dónde debemos EMPEZAR a contar?
+// ¿Dónde está la línea de LLEGADA?
+// ¿Cuál es el TAMAÑO DE PASO que debemos usar?
+
+//    START         FINISH      STEP SIZE
+for (let count = 1; count < 11; count += 1) {
+  console.log(count);
+}
