75448fbd1a2e0829ead349281e7c12291fdf91f5
ElVicDev <vicesc820@gmail.com>
2024-10-09 17:05:41 -0600

    Write your first loop

--- SCRIMBA/JavaScript/08_For/script.js
+++ SCRIMBA/JavaScript/08_For/script.js
@@ -10,3 +10,9 @@
 for (let count = 1; count < 11; count += 1) {
   console.log(count);
 }
+
+// *** Crea un bucle for que cuente de 10 a 100 en pasos de 10
+// *** Usa console.log para registrar los números
+for (let i = 10; i < 101; i += 10) {
+  console.log(i);
+}
