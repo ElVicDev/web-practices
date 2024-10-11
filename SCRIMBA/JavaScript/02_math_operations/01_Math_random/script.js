e099c9633dc9d985c769026eb5ef04cd2f366cd9
ElVicDev <vicesc820@gmail.com>
2024-10-11 00:34:12 -0600

    Generating random numbers

---
+++ SCRIMBA/JavaScript/02_math_operations/01_Math_random/script.js
@@ -0,0 +1,14 @@
+let randomNumber = Math.random() * 6;
+
+console.log(randomNumber);
+
+/* 
+What does Math.random() do?
+Tu respuesta:
+Nos dá un número aleatorio entre 0 y 1 (no incluyendo el 1)
+*/
+
+/*
+Si multiplicamos Math.random por 6
+Nos dará un número entre el 0 y el 6 (No incluyendo el 6)
+*/
