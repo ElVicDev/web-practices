dd28af0dc65988952e38ef109aeb3105f9b865bf
ElVicDev <vicesc820@gmail.com>
2024-10-11 14:18:29 -0600

    Intro to objects

---
+++ SCRIMBA/JavaScript/09_Objects/01_Intro/scrpt.js
@@ -0,0 +1,16 @@
+// * Objetos: almacenan datos en profundidad:
+// * tipos de datos compuestos/complejos
+// * pares clave-valor
+
+let course = {
+  title: "Learn CSS Grid for free",
+  lessons: 16,
+  creator: "Per Harald Borgen",
+  length: 63,
+  level: 2,
+  isFree: true,
+  tags: ["html", "css"],
+};
+
+console.log(course.length);
+console.log(course.tags);
