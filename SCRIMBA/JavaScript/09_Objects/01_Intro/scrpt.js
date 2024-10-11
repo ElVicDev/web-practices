9f13d6d628cbc275b8f3c2cbd2545926ad18e403
ElVicDev <vicesc820@gmail.com>
2024-10-11 14:27:59 -0600

    Create your first object

--- SCRIMBA/JavaScript/09_Objects/01_Intro/scrpt.js
+++ SCRIMBA/JavaScript/09_Objects/01_Intro/scrpt.js
@@ -14,3 +14,18 @@ let course = {
 
 console.log(course.length);
 console.log(course.tags);
+
+// * ----------------------------------------------------------------------------
+// * Crea un objeto que represente un anuncio de castillo de Airbnb.
+// * Debe contener al menos un valor booleano, una cadena, un número y una matriz
+// * Elimina al menos dos de las claves usando la notación de punto
+// * ----------------------------------------------------------------------------
+let castle = {
+  title: "Live like a king in my castle",
+  price: 190,
+  isSuperHost: true,
+  images: ["img/castle1.png", "img/caste2.png"],
+};
+
+console.log(castle.price);
+console.log(castle.isSuperHost);
