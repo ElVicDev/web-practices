8a9164d2d40dbf76e460bab07157b9dfec6a9a32
ElVicDev <vicesc820@gmail.com>
2024-10-10 21:21:50 -0600

    Use a function to set the card values

---
+++ SCRIMBA/JavaScript/03_Functions/Returning_Values/script.js
@@ -0,0 +1,28 @@
+let player1Time = 102;
+let player2Time = 107;
+
+// *   cmd+d - ctrl+d - Selecciona varias lineas para poderlas modificar al mismo tiempo
+
+function getFastestRaceTime() {
+  if (player1Time < player2Time) {
+    return player1Time;
+  } else if (player2Time < player1Time) {
+    return player2Time;
+  } else {
+    return player1Time;
+  }
+}
+
+// let fastestRace = getFastestRaceTime()
+// console.log(fastestRace)
+
+// Escribe una función que devuelva el tiempo total de la carrera
+// Llama/invoca la función y almacena el valor devuelto en una nueva variable
+// Finalmente, registra la variable
+function getTotalRaceTime() {
+  return player1Time + player2Time;
+}
+
+let totalTime = getTotalRaceTime();
+
+console.log(totalTime);
