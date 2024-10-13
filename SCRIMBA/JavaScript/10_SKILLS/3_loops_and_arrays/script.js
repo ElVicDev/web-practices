a8b2a885cff712cbd50907e7b5a42789adee25a9
ElVicDev <vicesc820@gmail.com>
2024-10-13 15:36:34 -0600

    Loops and Arrays

---
+++ SCRIMBA/JavaScript/10_SKILLS/3_loops_and_arrays/script.js
@@ -0,0 +1,15 @@
+let largeCountries = ["China", "India", "USA", "Indonesia", "Pakistan"];
+
+/* Use un bucle for para registrar lo siguiente en la consola:
+
+Los 5 países más grandes del mundo:
+- China
+- India
+- Estados Unidos
+- India
+- Pakistán
+*/
+console.log("The 5 largest countries in the world:");
+for (let i = 0; i < largeCountries.length; i++) {
+  console.log("-" + largeCountries[i]);
+}
