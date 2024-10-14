9121452c40d7dfbcc8f710c02498e3c050447475
ElVicDev <vicesc820@gmail.com>
2024-10-13 19:57:05 -0600

    Logical Operators

---
+++ SCRIMBA/JavaScript/10_SKILLS/5_logical_operators/script.js
@@ -0,0 +1,9 @@
+let dayOfMonth = 13;
+let weekday = "Friday";
+
+// Si es viernes 13, cierra la sesión con esta cara espeluznante: 😱
+// Usa el operador lógico "AND" -> &&
+
+if (dayOfMonth === 13 && weekday === "Friday") {
+  console.log("😱");
+}
