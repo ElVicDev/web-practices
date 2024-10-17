236c43343d2f2798a3b189d25ad43ca6ce0eb0da
ElVicDev <vicesc820@gmail.com>
2024-10-16 23:44:42 -0600

    Write your first string variable

---
+++ SCRIMBA/JavaScript/00_BASICS/01_string/script.js
@@ -0,0 +1,9 @@
+// * Crea una variable, messageToUser,
+// * que contiene el mensaje que hemos registrado
+
+let username = "ElVicDev";
+let message = "You have tree new notifications";
+
+let messageToUser = message + ", " + username + "!";
+
+console.log(messageToUser);
