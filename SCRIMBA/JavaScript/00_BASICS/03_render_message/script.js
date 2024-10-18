4a22261b7bd930dbee085c2f2a9de8f5202a1821
ElVicDev <vicesc820@gmail.com>
2024-10-18 01:24:55 -0600

    Improve the message with string concatenation

--- SCRIMBA/JavaScript/00_BASICS/03_render_message/script.js
+++ SCRIMBA/JavaScript/00_BASICS/03_render_message/script.js
@@ -1,12 +1,22 @@
+// * ------------------------
 // * Render a welcome message
+// * ------------------------
 
 // Toma el párrafo welcome-el y almacénalo en una variable llamada welcomeEl
 let welcomeEl = document.getElementById("welcome-el");
 
 // Crea dos variables (nombre y saludo) que contengan tu nombre
 // y el saludo que queremos mostrar en la página
-let name = "ElVicDev";
+let namePerson = "ElVicDev";
 let greeting = "Bienvenido ";
 
 // Muestra el mensaje de bienvenida usando welcomeEl.innerText
-welcomeEl.innerText = greeting + name;
+welcomeEl.innerText = greeting + namePerson;
+
+// * -------------------------------------------------
+// * Mejore el mensaje con la concatenación de cadenas
+// * -------------------------------------------------
+//  ¡Agrega un emoji al final! 👋
+//  SUGERENCIA: count = count + 1
+let welcomeEl2 = document.getElementById("welcome-el2");
+welcomeEl2.innerText = welcomeEl.innerText + " " + "👋";
