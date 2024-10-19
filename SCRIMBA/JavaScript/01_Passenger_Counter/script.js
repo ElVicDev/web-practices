1d9df3ac977e7fe357bbb4fa03f38d0237829321
ElVicDev <vicesc820@gmail.com>
2024-10-18 23:32:58 -0600

    Create the save feature

--- SCRIMBA/JavaScript/01_Passenger_Counter/script.js
+++ SCRIMBA/JavaScript/01_Passenger_Counter/script.js
@@ -1,19 +1,25 @@
 let count = 0;
+// 1. Tome el párrafo save-el y almacénelo en una variable llamada saveEl
 let saveEl = document.getElementById("save-el");
 let countEl = document.getElementById("count-el");
 
 function increment() {
+  // Cambia esto para utilizar la técnica más igual que has aprendido.
+  // count = count + 1
   count += 1;
   countEl.textContent = count;
 }
 
 // Crea una función, save(), que registra el recuento cuando se la llama
 function save() {
-  // console.log(count)
+  // 2. Crea una variable que contenga tanto el recuento como el separador de guiones, es decir, "12 - "
+  // 3. Representa la variable en saveEl usando innerText
+  // Nota: Asegúrate de no eliminar el contenido existente del párrafo
   let countStr = count + " - ";
-  saveEl.textContenttStr;
-  countEl.textContent = 0;
-  count = 0;
+  saveEl.innerText += countStr;
+  // countEl.textContent = 0;
+  // count = 0;
+  // console.log(count)
 }
 
 console.log("Let's count people on the subway!");
