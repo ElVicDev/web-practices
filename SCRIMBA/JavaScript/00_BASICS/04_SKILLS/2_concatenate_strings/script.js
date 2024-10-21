d39f8d74911477550ab110cb4964ff84e8fe24fc
ElVicDev <vicesc820@gmail.com>
2024-10-20 23:45:40 -0600

    Concatenate two strings in a function

---
+++ SCRIMBA/JavaScript/00_BASICS/04_SKILLS/2_concatenate_strings/script.js
@@ -0,0 +1,13 @@
+// * -------------------------------------
+// * Concatenar dos cadenas en una función
+// * -------------------------------------
+
+let namePerson = "Linda";
+let greeting = "Hi there";
+
+// Crea una función que cierre la sesión "¡Hola, Linda!" cuando se la llama
+function greetLinda() {
+  console.log(greeting + ", " + namePerson + "!");
+}
+
+greetLinda();
