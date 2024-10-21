47ef9734c7e3dff16bc225b768593f79ec3d3621
ElVicDev <vicesc820@gmail.com>
2024-10-20 23:34:19 -0600

    Variables practice

---
+++ SCRIMBA/JavaScript/00_BASICS/04_SKILLS/1_variables/script.js
@@ -0,0 +1,12 @@
+// * ------------------
+// * Variables practice
+// * ------------------
+
+// Crea dos variables, firstName y lastName
+// Concatena las dos variables en una tercera variable llamada fullName
+// Registra fullName en la consola
+
+let firstName = "El Vic";
+let lastName = "Developer";
+let fullName = firstName + " " + lastName;
+console.log(fullName);
