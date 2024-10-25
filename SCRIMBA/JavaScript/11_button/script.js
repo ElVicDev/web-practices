4d31e03467caf317ecd213f644a7363046c1bc0b
ElVicDev <vicesc820@gmail.com>
2024-10-24 23:09:01 -0600

    Create the myLeads array and inputEl

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -1,8 +1,10 @@
-/* function saveLead() {
-  console.log("Button clicked!");
-} */
+// Crea dos variables:
+// myLeads -> debe asignarse a una matriz vacía
+// inputEl -> debe asignarse al campo de entrada de texto
 
-let inputBtn = document.getElementById("input-btn");
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn");
 
 inputBtn.addEventListener("click", function () {
   console.log("Button clicked from addEventListener");
