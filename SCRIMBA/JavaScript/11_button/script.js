4fea7a1b8e4fceba371e61ac5aef795505792cd3
ElVicDev <vicesc820@gmail.com>
2024-10-25 18:24:05 -0600

    Create the unordered list

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -5,6 +5,8 @@ let myLeads = [
 ];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+// 2. Toma la lista desordenada y almacénala en una variable constante llamada ulEl
+const ulEl = document.getElementById("ul-el");
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
