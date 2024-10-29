0d0d156dc6da74d803460887ec2719663b3d794a
ElVicDev <vicesc820@gmail.com>
2024-10-29 13:05:27 -0600

    Make the template string even more dynamic

--- SCRIMBA/JavaScript/15_template_string/script.js
+++ SCRIMBA/JavaScript/15_template_string/script.js
@@ -8,6 +8,11 @@ const recipient = "James";
 
 // Refactor the email string to use template strings
 // const email = "Hey " + recipient + "! How is it going? Cheers Per";
-const email = `Hey ${recipient}! How is it going? Cheers Per`;
+// const email = `Hey ${recipient}! How is it going? Cheers Per`;
 
+/* Haga que la cadena de plantillas sea aún más dinámica
+Crea una nueva variable, sender, y establece su valor en tu nombre
+Utiliza tu variable sender en lugar de "Per" */
+const sender = "ElVicDev";
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`;
 console.log(email);
