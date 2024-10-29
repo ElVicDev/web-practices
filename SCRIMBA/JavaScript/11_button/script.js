de7f0aec77d37c0ac3c2bee6703f189ab92534ea
ElVicDev <vicesc820@gmail.com>
2024-10-29 17:46:52 -0600

    Get the leads from localStorage

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -3,6 +3,12 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// Obtener los clientes potenciales del almacenamiento local - PS: JSON.parse()
+// Almacenarlos en una variable, leadsFromLocalStorage
+// Cerrar sesión en la variable
+localStorage.clear();
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
