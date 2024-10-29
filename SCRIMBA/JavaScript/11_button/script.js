8910a6109536facfc61a80a1ebac3ab9f7da51e2
ElVicDev <vicesc820@gmail.com>
2024-10-29 16:51:15 -0600

    Deploying the Chrome Extension

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -3,6 +3,24 @@ const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 
+// * ----------------------
+// * Tu primer localStorage
+// * ----------------------
+// 1. Guarda un par clave-valor en localStorage
+// 2. Actualiza la página. Obtén el valor y regístralo en la consola
+// 3. Limpia localStorage
+
+// SUGERENCIAS:
+// localStorage.setItem(key, value)
+// localStorage.getItem(key)
+// localStorage.clear()
+// PD: tanto la clave como el valor deben ser cadenas
+
+// localStorage.setItem("repository", "https://www.elvicdev.com/");
+// let elvicdev = localStorage.getItem("repository");
+// console.log(elvicdev);
+// localStorage.clear();
+
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   console.log(myLeads);
