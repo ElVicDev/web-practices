eba6c6eea83269b8a7847d674b1e6edfa1d5e1ff
ElVicDev <vicesc820@gmail.com>
2024-11-01 10:24:12 -0600

    How function parameters can improve our code

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -1,47 +1,39 @@
 let myLeads = [];
+let oldLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
-// 1. Almacene el botón eliminar en una variable deleteBtn
 const deleteBtn = document.getElementById("delete-btn");
-
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
-  renderLeads();
+  render(myLeads);
 }
 
-// 2. Escucha los dobles clics en el botón de eliminar (¡búscalo en Google!)
-// 3. Cuando hagas clic, borra localStorage, myLeads y el DOM
+function render(leads) {
+  let listItems = "";
+  for (let i = 0; i < leads.length; i++) {
+    listItems += `
+      <li>
+        <a target='_blank' href='${leads[i]}'>
+          ${leads[i]}
+        </a>
+      </li>
+    `;
+  }
+  ulEl.innerHTML = listItems;
+}
+
 deleteBtn.addEventListener("dblclick", function () {
   localStorage.clear();
   myLeads = [];
-  renderLeads();
+  render(myLeads);
 });
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
   inputEl.value = "";
-  // Guarda la matriz myLeads en localStorage
-  // PD: recuerda JSON.stringify()
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
-  renderLeads();
-
-  // Para verificar que funciona:
-  console.log(localStorage.getItem("myLeads"));
+  render(myLeads);
 });
-
-function renderLeads() {
-  let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
-    listItems += `
-            <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
-                </a>
-            </li>
-        `;
-  }
-  ulEl.innerHTML = listItems;
-}
