66e3a77b8c6a1b76d417d7c9787b3109e7c12881
ElVicDev <vicesc820@gmail.com>
2024-11-01 10:10:42 -0600

    Make the delete button work

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -2,13 +2,23 @@ let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+// 1. Almacene el botón eliminar en una variable deleteBtn
+const deleteBtn = document.getElementById("delete-btn");
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
   myLeads = leadsFromLocalStorage;
   renderLeads();
 }
+
+// 2. Escucha los dobles clics en el botón de eliminar (¡búscalo en Google!)
+// 3. Cuando hagas clic, borra localStorage, myLeads y el DOM
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  renderLeads();
+});
 
 inputBtn.addEventListener("click", function () {
   myLeads.push(inputEl.value);
