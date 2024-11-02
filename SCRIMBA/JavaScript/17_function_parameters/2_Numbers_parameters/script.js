14686038d34c2ea4922442abda76897c76f35fc3
ElVicDev <vicesc820@gmail.com>
2024-11-02 00:28:58 -0600

    Numbers as function parameters

---
+++ SCRIMBA/JavaScript/17_function_parameters/2_Numbers_parameters/script.js
@@ -0,0 +1,12 @@
+// * ----------------------------------
+// * Números como parámetros de función
+// * ----------------------------------
+
+// Crea una función, add(), que suma dos números y devuelve la suma.
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(add(3, 4)); // Debería registrar 7
+console.log(add(9, 102)); // Debería registrar 111
