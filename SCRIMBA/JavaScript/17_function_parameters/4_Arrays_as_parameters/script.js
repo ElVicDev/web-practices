87091f7644a93f3d1fc88f120435115d5f70e99f
ElVicDev <vicesc820@gmail.com>
2024-11-02 08:55:02 -0600

    Arrays as parameters

--- SCRIMBA/JavaScript/17_function_parameters/4_Arrays_as_parameters/script.js
+++ SCRIMBA/JavaScript/17_function_parameters/4_Arrays_as_parameters/script.js
@@ -7,7 +7,7 @@ function getFirst(arr) {
   return arr[0];
 }
 
+// Llámala con una matriz como argumento para verificar que funciona
 let firstCard = getFirst([10, 2, 5]);
 
-// Llámala con una matriz como argumento para verificar que funciona
 console.log(firstCard);
