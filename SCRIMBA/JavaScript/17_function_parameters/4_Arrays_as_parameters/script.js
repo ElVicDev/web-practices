3bee2fc251c8c3e4551f50d752095fc5cd4464b2
ElVicDev <vicesc820@gmail.com>
2024-11-02 08:51:52 -0600

    Arrays as parameters

---
+++ SCRIMBA/JavaScript/17_function_parameters/4_Arrays_as_parameters/script.js
@@ -0,0 +1,13 @@
+// * ------------------------
+// * Matrices como parámetros
+// * ------------------------
+
+// Crea una función, getFirst(arr), que devuelva el primer elemento de la matriz
+function getFirst(arr) {
+  return arr[0];
+}
+
+let firstCard = getFirst([10, 2, 5]);
+
+// Llámala con una matriz como argumento para verificar que funciona
+console.log(firstCard);
