179c78186264a30054e9973c47cbad1b0ad4fabf
ElVicDev <vicesc820@gmail.com>
2024-11-02 11:05:26 -0600

    Save the tab url

--- SCRIMBA/JavaScript/11_button/script.js
+++ SCRIMBA/JavaScript/11_button/script.js
@@ -16,7 +16,11 @@ if (leadsFromLocalStorage) {
 const tabs = [{ url: "https://www.linkedin.com/in/vicente-escobar" }];
 // 2. Escucha los clics en tabBtn. Registra la URL de LinkedIn en la consola
 tabBtn.addEventListener("click", function () {
-  console.log(tabs[0].url);
+  // Guarde la URL en lugar de cerrar la sesión
+  // console.log(tabs[0].url);
+  myLeads.push(tabs[0].url);
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
 });
 
 function render(leads) {
