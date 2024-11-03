833a9b3ecd35a059f483eb1b3d7afbd56bf6cc82
ElVicDev <vicesc820@gmail.com>
2024-11-03 15:27:53 -0600

    save to localStorage

---
+++ SCRIMBA/JavaScript/18 SKILLS/3_localStoreage/script.js
@@ -0,0 +1,12 @@
+// * -------------------------------
+// * Guardar en almacenamiento local
+// * -------------------------------
+
+// Guardar un valor en localStorage
+// Eliminar el código y actualizar la página
+// Obtener el valor de localStorage y cerrar la sesión
+
+localStorage.setItem("myCredits", "100");
+
+let myCredits = localStorage.getItem("myCredits");
+console.log(myCredits);
