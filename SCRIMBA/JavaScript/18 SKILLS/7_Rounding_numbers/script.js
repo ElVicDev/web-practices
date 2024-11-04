dc16220b64ce50498643360a1c416ec420edc540
ElVicDev <vicesc820@gmail.com>
2024-11-03 20:34:08 -0600

    Rounding numbers

---
+++ SCRIMBA/JavaScript/18 SKILLS/7_Rounding_numbers/script.js
@@ -0,0 +1,11 @@
+// * -------------------
+// * Redondeo de números
+// * -------------------
+
+// Desafío:
+// Redondea el precio que aparece en el botón a dos decimales.
+// ¿No sabes qué método usar? ¡Búscalo en Google!
+
+const totalPrice = 420.69235632455;
+const btn = document.getElementById("purchase-btn");
+btn.textContent = `Buy €${totalPrice.toFixed(2)}`;
