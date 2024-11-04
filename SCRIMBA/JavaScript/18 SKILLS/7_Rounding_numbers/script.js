9c35b30e7ea2706e852550ba2983bad81a95947a
ElVicDev <vicesc820@gmail.com>
2024-11-03 20:36:53 -0600

    Rounding numbers

--- SCRIMBA/JavaScript/18 SKILLS/7_Rounding_numbers/script.js
+++ SCRIMBA/JavaScript/18 SKILLS/7_Rounding_numbers/script.js
@@ -9,3 +9,7 @@
 const totalPrice = 420.69235632455;
 const btn = document.getElementById("purchase-btn");
 btn.textContent = `Buy €${totalPrice.toFixed(2)}`;
+
+// * -----------------------------------------------
+// * NOTA: toFixed() redondea el número hacia arriba
+// * -----------------------------------------------
