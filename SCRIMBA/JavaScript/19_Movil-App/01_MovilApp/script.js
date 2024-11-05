25d6469e6ab02a29b071e35740653fdb0b64ef76
ElVicDev <vicesc820@gmail.com>
2024-11-05 01:02:13 -0600

    Create new Firebase Realtime Database

--- SCRIMBA/JavaScript/19_Movil-App/01_MovilApp/script.js
+++ SCRIMBA/JavaScript/19_Movil-App/01_MovilApp/script.js
@@ -1,10 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
-const firebaseConfig = {};
+const firebaseConfig = {
+  databaseURL: "https://leads-tracker-app-4a42d-default-rtdb.firebaseio.com/",
+};
 
 const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
 
-console.log(app);
+console.log(database);
 
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
