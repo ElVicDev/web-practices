db8bbbf112c0fe928aafefb2f9328d347269fed8
ElVicDev <vicesc820@gmail.com>
2024-11-05 23:25:26 -0600

    Push data to Firebase Realtime Database

---
+++ SCRIMBA/JavaScript/19_Movil-App/2_push_data/script.js
@@ -0,0 +1,23 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
+import {
+  getDatabase,
+  ref,
+  push,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
+
+const firebaseConfig = {
+  databaseURL: "https://birthday-app-975d5-default-rtdb.firebaseio.com/",
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+console.log(database);
+const referenceInDB = ref(database, "birthdays");
+
+const birthdayInputField = document.getElementById("birthday-input");
+const submitButton = document.getElementById("submit-button");
+
+submitButton.addEventListener("click", function () {
+  push(referenceInDB, birthdayInputField.value);
+  birthdayInputField.value = "";
+});
