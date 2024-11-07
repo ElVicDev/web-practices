0ce116dd28d1ff52bf70b0609ec67ee6cc96ab8f
ElVicDev <vicesc820@gmail.com>
2024-11-06 20:42:20 -0600

    Console log data inside of onValue

--- SCRIMBA/JavaScript/19_Movil-App/01_MovilApp/script.js
+++ SCRIMBA/JavaScript/19_Movil-App/01_MovilApp/script.js
@@ -3,6 +3,7 @@ import {
   getDatabase,
   ref,
   push,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-database.js";
 
 const firebaseConfig = {
@@ -34,6 +35,12 @@ function render(leads) {
   }
   ulEl.innerHTML = listItems;
 }
+
+// Desafío: Cerrar la sesión de una instantánea de su base de datos
+// cuando se le agregue un nuevo valor
+onValue(referenceInDB, function (snapshot) {
+  console.log(snapshot.val());
+});
 
 deleteBtn.addEventListener("dblclick", function () {});
 
