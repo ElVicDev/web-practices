92659c60d8d3e9b72c5a4012c0a19e191796b8d9
ElVicDev <vicesc820@gmail.com>
2024-11-06 21:05:32 -0600

    Turn an Object into an Array

---
+++ SCRIMBA/JavaScript/19_Movil-App/4_objet_into_an_array/script.js
@@ -0,0 +1,14 @@
+const loginCredentials = {
+  rafidhoda: "BestPassword123",
+  shahrukhkhan: "InBigCitiesSmallThingsHappen",
+  jackblack: "ThisIsNotTheGreatestPasswordInTheWorld",
+};
+
+// Obtenemos los usuarios (keys)
+console.log(Object.keys(loginCredentials));
+
+// Obtenemos las contraseñas (values)
+console.log(Object.values(loginCredentials));
+
+// Obtenemos usuarios y contraseñas (entries)
+console.log(Object.entries(loginCredentials));
