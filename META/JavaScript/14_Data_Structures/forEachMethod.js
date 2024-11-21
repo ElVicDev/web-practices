00dc557e56a0f0c683310f7aaa9784776562b608
ElVicDev <vicesc820@gmail.com>
2024-11-20 23:40:27 -0600

    The forEach() method

---
+++ META/JavaScript/14_Data_Structures/forEachMethod.js
@@ -0,0 +1,9 @@
+// * --------------------
+// * The forEach() method
+// * --------------------
+
+const fruits = ["kiwi", "mango", "apple", "pear"];
+function appendIndex(fruit, index) {
+  console.log(`${index}. ${fruit}`);
+}
+fruits.forEach(appendIndex);
