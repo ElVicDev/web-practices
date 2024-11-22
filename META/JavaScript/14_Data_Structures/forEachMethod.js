dd65b7eeb7fe9884a4c2b6dce38dd937837a0b38
ElVicDev <vicesc820@gmail.com>
2024-11-21 23:18:05 -0600

    The forEach() method

--- META/JavaScript/14_Data_Structures/forEachMethod.js
+++ META/JavaScript/14_Data_Structures/forEachMethod.js
@@ -7,3 +7,18 @@ function appendIndex(fruit, index) {
   console.log(`${index}. ${fruit}`);
 }
 fruits.forEach(appendIndex);
+
+// * ---------------------------------------------------
+
+/*  Muy a menudo, la función que el métodoforEach() 
+    necesita utilizar se pasa directamente en la llamada 
+    al método, como en este caso: */
+
+const veggies = ["onion", "garlic", "potato"];
+veggies.forEach(function (veggie, index) {
+  console.log(`${index}. ${veggie}`);
+});
+
+/*  Esto hace que el código sea más compacto, pero quizás
+    algo más difícil de leer. Para aumentar la legibilidad,
+    a veces se utilizan funciones de flecha. */
