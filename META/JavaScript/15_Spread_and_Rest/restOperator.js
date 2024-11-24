cc45060930b235bf354e9e65385477827c9f918e
ElVicDev <vicesc820@gmail.com>
2024-11-23 23:49:12 -0600

    Using Rest Operator

---
+++ META/JavaScript/15_Spread_and_Rest/restOperator.js
@@ -0,0 +1,15 @@
+// * -----------------
+// * The rest operator
+// * -----------------
+
+// Utilizando rest operator, es fácil concatenar matrices:
+const fruits = ["apple", "pear", "plum"];
+const berries = ["blueberry", "strawberry"];
+const fruitsAndBerries = [...fruits, ...berries]; // concatenate
+console.log(fruitsAndBerries); // outputs a single array
+
+// También es fácil unir objetos:
+const flying = { wings: 2 };
+const car = { wheels: 4 };
+const flyingCar = { ...flying, ...car };
+console.log(flyingCar); // {wings: 2, wheels: 4}
