779b470789223bd2621ff6e5a9c3e39740bb13c5
ElVicDev <vicesc820@gmail.com>
2024-12-11 23:53:24 -0600

    Default Parameters

---
+++ META/JavaScript/12_OOP/parameters.js
@@ -0,0 +1,7 @@
+//  *** Parámetros por defecto ***
+
+// Función sin parámetros por defecto establecidos:
+function noDefaultParams(number) {
+  console.log("Result:", number * number);
+}
+noDefaultParams(); // Result: NaN
