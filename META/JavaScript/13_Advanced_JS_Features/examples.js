1281a281513b2ba064d88ccb5607e46d1ff5f6c0
ElVicDev <vicesc820@gmail.com>
2024-12-15 00:42:02 -0600

    Loop over any object's own property keys and values - Two Examples

---
+++ META/JavaScript/13_Advanced_JS_Features/examples.js
@@ -0,0 +1,34 @@
+// * --------------------------------------------
+// * EXAMPLE 1
+// * --------------------------------------------
+
+var clothingItem = {
+  price: 50,
+  color: "beige",
+  material: "cotton",
+  season: "autumn",
+};
+
+for (const key of Object.keys(clothingItem)) {
+  console.log(key, ":", clothingItem[key]);
+}
+
+// * --------------------------------------------
+// * EXAMPLE 2
+// * --------------------------------------------
+function testBracketsDynamicAccess() {
+  var dynamicKey;
+  if (Math.random() > 0.5) {
+    dynamicKey = "speed";
+  } else {
+    dynamicKey = "color";
+  }
+
+  var drone = {
+    speed: 15,
+    color: "orange",
+  };
+
+  console.log(drone[dynamicKey]);
+}
+testBracketsDynamicAccess(); // 15
