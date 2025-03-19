69aeceec3c02ac7d939c3c1c4a3bc6933cf6e36b
ElVicDev <vicesc820@gmail.com>
2025-03-19 00:34:52 -0600

    Nested Example

---
+++ WD4D/3_JavaScript/12_name/2_nested_with_name/js/NestedName.js
@@ -0,0 +1,8 @@
+var name = prompt("What is your name?");
+
+if (name.length != 0) {
+  if (name == "Colleen") document.writeln("What a beautiful name");
+  else document.writeln("Hello " + name);
+} else document.writeln("Feeling shy?");
+
+/*Una vez que se cumple un condicional, se pueden omitir el resto*/
