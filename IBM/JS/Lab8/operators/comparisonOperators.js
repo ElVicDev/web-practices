d11103396003dfcbcc8e76d339a0588a54e8f507
ElVicDev <vicesc820@gmail.com>
2025-06-27 00:25:39 -0600

    Comparison Operators

---
+++ IBM/JS/Lab8/operators/comparisonOperators.js
@@ -0,0 +1,23 @@
+/* Los operadores de comparación se utilizan para comparar valores o variables con otros valores o variables
+•	El operador == verifica si el operando a la izquierda es igual en valor al operando a la derecha
+•	El operador === verifica si el operando a la izquierda es igual en valor y en tipo al operando a la derecha
+•	El operador != verifica si el operando a la izquierda no es igual en valor al operando a la derecha
+•	El operador > verifica si el operando a la izquierda es mayor que el de la derecha
+•	El operador < verifica si el operando a la izquierda es menor que el de la derecha
+•	El operador >= verifica si el operando a la izquierda es mayor o igual que el de la derecha
+•	El operador <= verifica si el operando a la izquierda es menor o igual que el de la derecha */
+
+console.log("5=='5' ", 5 == "5");
+console.log("5==='5' ", 5 === "5");
+console.log("5===5 ", 5 === 5);
+console.log("5 != 5 ", 5 !== 5);
+console.log("5 != 6 ", 5 != 6);
+console.log("5 != '5' ", 5 !== "5");
+console.log("5 > 2 ", 5 > 2);
+console.log("5 > 7 ", 5 > 7);
+console.log("5 > 5 ", 5 > 5);
+console.log("5 < 7 ", 5 < 7);
+console.log("5 < 2 ", 5 < 2);
+console.log("5 < 5 ", 5 < 5);
+console.log("5 >= 5 ", 5 >= 5);
+console.log("5 <= 5 ", 5 <= 5);
