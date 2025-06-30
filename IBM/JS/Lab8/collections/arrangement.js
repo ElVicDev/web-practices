206adaa5207f7517b78ba1cbe3ccd2b0bcfce172
ElVicDev <vicesc820@gmail.com>
2025-06-29 23:02:33 -0600

    Collections - Arrangement

---
+++ IBM/JS/Lab8/collections/arrangement.js
@@ -0,0 +1,9 @@
+/* Un arreglo es una colección indexada.
+    Las posiciones de índice comienzan desde 0.
+    El elemento en la primera posición está en el índice 0,
+    el segundo elemento está en la posición 1, y así sucesivamente.
+    El índice de la última posición siempre será uno menos que la longitud del arreglo. */
+
+let myArray = ["Jack", "Jill", 4, 5, true, "John"];
+console.log(myArray[0]);
+console.log(myArray[5]);
