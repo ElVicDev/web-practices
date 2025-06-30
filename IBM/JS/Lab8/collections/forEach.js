f6dba0754c577fce2402051033aa9878978a2cbe
ElVicDev <vicesc820@gmail.com>
2025-06-29 23:04:39 -0600

    Collections - forEach

---
+++ IBM/JS/Lab8/collections/forEach.js
@@ -0,0 +1,7 @@
+/* Para iterar a través de arreglos, hay un tipo especial de bucle for, forEach,
+que se ejecuta para cada valor en el arreglo dado. */
+
+let myArray = ["Jack", "Jill", 4, 5, true, "John"];
+myArray.forEach((element) => {
+  console.log(element);
+});
