67e26efe7a287a2fba42edfd91c60c2602ec9bdb
ElVicDev <vicesc820@gmail.com>
2025-07-13 13:40:34 -0600

    Hello world in C

---
+++ IBM/Labs/Hello World/C/helloworld.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+  printf("Hello World!\n");
+  return 0;
+}
+
+/*  Compilar el código usando:
+    gcc -o hello helloworld.c
+    -o especifica el nombre del archivo de salida
+    Para ejecutar el archivo compilado teclear:
+    ./hello   */
