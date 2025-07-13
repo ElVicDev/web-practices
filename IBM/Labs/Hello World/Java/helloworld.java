1cea6c8b23381901fa3408a5f612b5a8c9915dfb
ElVicDev <vicesc820@gmail.com>
2025-07-13 17:12:45 -0600

    Hello world in Java

---
+++ IBM/Labs/Hello World/Java/helloworld.java
@@ -0,0 +1,16 @@
+public class helloworld {
+    public static void main(String s[]) {
+        System.out.println("Hello World!");
+    }
+}
+
+/*
+ * Compilar el código usando:
+ * javac helloworld.java
+ * javac es un compilador de programas Java.
+ * Por defecto, la salida de la compilación
+ * se almacena en un archivo con el mismo nombre
+ * y la extensión .class.
+ * Para ejecutar el archivo compilado teclear:
+ * java helloworld
+ */
