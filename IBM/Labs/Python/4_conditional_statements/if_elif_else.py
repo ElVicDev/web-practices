f6305c4221c517200d599b0da1de7e9f9d12cae9
ElVicDev <vicesc820@gmail.com>
2025-07-14 00:42:56 -0600

    Exercise 4: Execute Conditional Statements - if-elif-else Example

---
+++ IBM/Labs/Python/4_conditional_statements/if_elif_else.py
@@ -0,0 +1,9 @@
+""" Execute Conditional Statements - if-elif-else Example """
+
+score=88
+if score>90:
+    print("Well done! That gets you an A+ grade")
+elif score>=70:
+    print("Good work! Passed the test!")
+else:
+    print("Better luck next time!")
