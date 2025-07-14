cbbcf08b2d560cea4c45f005ce67abc91d94d5ab
ElVicDev <vicesc820@gmail.com>
2025-07-14 01:10:11 -0600

    Exercise 6: Execute a Break Statement using a For-Loop

---
+++ IBM/Labs/Python/6_break_statement/break.py
@@ -0,0 +1,7 @@
+""" Exercise 6: Execute a Break Statement using a For-Loop """
+
+shopping_list = ["bread", "water", "chips", "candy", "soda"]
+for x in shopping_list:
+  print(x)
+  if x == "chips":
+    break
