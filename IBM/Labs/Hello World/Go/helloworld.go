cb62a82f45b144f1facc10ef5df7229de7766417
ElVicDev <vicesc820@gmail.com>
2025-07-13 18:31:36 -0600

    Hello world in Go

---
+++ IBM/Labs/Hello World/Go/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+func main() {
+    fmt.Println("Hello World!")
+}
+
+/* 	El código Go debe ser compilado y ejecutado 
+	como C y Java. 
+	Se puede hacer con un solo comando.
+	Para compilar y ejecutar el archivo compilado, escribe:
+	go run helloworld.go
+*/
