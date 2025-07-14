c18ca4a8592e1592d128c89d98d1b310efd1dab9
ElVicDev <vicesc820@gmail.com>
2025-07-13 19:59:12 -0600

    Hello world in C++

---
+++ IBM/Labs/Hello World/C++/helloworld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+    return 0;
+}
+
+/*
+ * Compilar el código usando:
+ * g++ -o helloworld_cpp helloworld.cpp
+ * -o especifica el nombre del archivo de salida
+ * Para ejecutar el archivo compilado teclear:
+ * ./helloworld_cpp
+ */
