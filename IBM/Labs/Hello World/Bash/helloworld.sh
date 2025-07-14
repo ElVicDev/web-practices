6132f481f7ebfb69f080b3c67417a2a4339fa353
ElVicDev <vicesc820@gmail.com>
2025-07-13 20:41:06 -0600

    Hello world in Bash

---
+++ IBM/Labs/Hello World/Bash/helloworld.sh
@@ -0,0 +1,10 @@
+# Los scripts de shell son exclusivos del sistema operativo UNIX. 
+# Es uno de los idiomas de secuencias de comandos más antiguos 
+# y se utiliza para ejecutar procedimientos del sistema predominantemente. 
+# Se ejecuta como es como otros idiomas de secuencias de comandos.
+
+echo "Hello World!"
+
+# No hay compilación para el código de Bash.
+# Para ejecutar el archivo escribe:
+# sh helloworld.sh
